71b7254e334e2502150054e639282d94dc892375
Sagarika Mohanty <sagarikamohanty.official@gmail.com>
2024-02-02 13:45:14 +0530

    userList_component

--- src/Components/User/UserList.js
+++ src/Components/User/UserList.js
@@ -1,18 +1,15 @@
-
-import Card from '../UI/Card';
-
+import Card from "../UI/Card";
+import UserDetail from "./UserDetail";
 const UserList = (props) => {
-    
-    const addUser =()=> {
-
-    }
-
-    return (
-        <Card>
-            <ul>
-
-            </ul>
+  return (
+    <ul style={{ listStyle: "none" }}>
+      {props.Userdetail.map((newUser) => (
+        <Card key={newUser.id}>
+          <UserDetail user={newUser} />
         </Card>
-    )
+      ))}
+    </ul>
+  );
+};
 
-}
+export default UserList;
